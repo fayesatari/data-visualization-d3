7c82f77b994a3062140c8fd35bd7d802cd28ccc0
Faye Satari <faye.satari@gmail.com>
2021-06-18 22:08:23 -0400

    First initialization

---
+++ src/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import * as helper from './scripts/helper.js'
+
+/**
+ * @file This file is the entry-point for the the code
+ * @version v1.0.0
+ */
+(function (d3) {
+  const svgSize = { width: 800, height: 600, }
+
+  build()
+
+  /**
+   *   This function builds the graph.
+   */
+  function build() {
+  }
+})(d3)
